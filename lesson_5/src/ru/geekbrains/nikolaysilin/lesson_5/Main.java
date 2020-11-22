cd6de024de806c916dc5e050cbf0e7619b74a919
NikolaySilin <silinnikolay@yandex.ru>
2020-11-22 20:51:10 +0500

    homework_5 Update

--- lesson_5/src/ru/geekbrains/nikolaysilin/lesson_5/Main.java
+++ lesson_5/src/ru/geekbrains/nikolaysilin/lesson_5/Main.java
@@ -1,5 +1,7 @@
 package ru.geekbrains.nikolaysilin.lesson_5;
 
+import java.util.Arrays;
+
 public class Main {
 
     static final int size = 10000000;
@@ -8,70 +10,78 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
+        Main Main = new Main();
+
         hasNoMultiThread();
 
-        hasMultiThread();
+        Main.hasMultiThread();
 
     }
 
     protected static void hasNoMultiThread() {
 
-        long a = System.currentTimeMillis();
+        System.out.println("1. Старт метода с одним потоком.");
+
         float[] arr = new float[size];
+        Arrays.fill(arr, 1.0f);
+
+        long start = System.currentTimeMillis();
         for(int i = 0; i < size; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
+        long end = System.currentTimeMillis();
 
-            System.out.println("Время выполнения программы без многопоточности: " + (System.currentTimeMillis() - a));
+            System.out.println("Завершение метода 1. Время выполнения программы с одним потоком: " + (end - start) + " миллисекунд.");
+        System.out.println();
 
     }
 
-    protected static void hasMultiThread () throws InterruptedException {
+    protected void hasMultiThread() throws InterruptedException {
 
-        float[] a1 = new float[half_size];
-        float[] a2 = new float[half_size];
-
-
-        long a = System.currentTimeMillis();
+        System.out.println("2. Старт метода с двумя потоками.");
 
         float[] arr = new float[size];
-        for(int i = 0; i < size; i++);
+        float[] arr1 = new float[half_size];
+        float[] arr2 = new float[half_size];
+        Arrays.fill(arr, 1.0f);
 
-        System.arraycopy(arr, 0, a1, 0, half_size);
-        System.arraycopy(arr, half_size, a2, 0, half_size);
+        long start = System.currentTimeMillis();
+        System.arraycopy(arr, 0, arr1, 0, half_size);
+        System.arraycopy(arr, half_size, arr2, 0, half_size);
+        long split = System.currentTimeMillis();
+        System.out.println("Время выполнения на разделение массива: " + (split - start) + " миллисекунд.");
 
-        Thread t_1 = new Thread(() -> {
-
-            for(int i = 0; i < half_size; i++) {
-                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-
-        });
-
-        Thread t_2 = new Thread(() -> {
-
-            for(int i = 0; i < half_size; i++) {
-                a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-
-        });
-
-        System.arraycopy(a1, 0, arr, 0, half_size);
-        System.arraycopy(a2, 0, arr, half_size, half_size);
+        Thread t_1 = new Thread(() -> this.twoThread(arr1, 1));
+        Thread t_2 = new Thread(() -> this.twoThread(arr2, 2));
 
         t_1.start();
         t_2.start();
 
-        if (t_1.isAlive()) t_1.join();
-        if (t_2.isAlive()) t_2.join();
+        t_1.join();
+        t_2.join();
 
-        System.out.println("Время выполнения программы c многопоточностью: " + (System.currentTimeMillis() - a));
+        long connect = System.currentTimeMillis();
+        System.arraycopy(arr1, 0, arr, 0, half_size);
+        System.arraycopy(arr2, 0, arr, half_size, half_size);
+        long end = System.currentTimeMillis();
+        System.out.println("Время выполнения на склеивание двух массивов в один: " + (end - connect) + " миллисекунд.");
 
+        System.out.println("Завершение метода 2. Время выполнения программы с двумя потоками: " + (end - start) + " миллисекунд.");
 
+    }
 
+    protected void twoThread(float[] arr, int number) {
 
+        long start = System.currentTimeMillis();
 
+        for (int i = 0; i < arr.length; i++) {
 
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println(String.format("Время выполнения потока: %d, равно: %s", number, String.valueOf(end - start) + " миллисекунд."));
     }
 
 }
