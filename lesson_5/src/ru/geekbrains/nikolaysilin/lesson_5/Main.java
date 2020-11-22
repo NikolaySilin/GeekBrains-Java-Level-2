8e23a7a0a72dcedf133f578dd22c665ff9973754
NikolaySilin <silinnikolay@yandex.ru>
2020-11-22 22:40:00 +0500

    homework_5 Four-Thread Update

--- lesson_5/src/ru/geekbrains/nikolaysilin/lesson_5/Main.java
+++ lesson_5/src/ru/geekbrains/nikolaysilin/lesson_5/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     static final int size = 10000000;
-    static final int half_size = size / 2;
+    static final int quarter_size = size / 4;
 
 
     public static void main(String[] args) throws InterruptedException {
@@ -31,46 +31,61 @@ public class Main {
         }
         long end = System.currentTimeMillis();
 
-            System.out.println("Завершение метода 1. Время выполнения программы с одним потоком: " + (end - start) + " миллисекунд.");
+        System.out.println("Время выполнения программы с одним потоком: " + (end - start) + " миллисекунд.");
+        System.out.println("Завершение метода 1.");
         System.out.println();
 
     }
 
     protected void hasMultiThread() throws InterruptedException {
 
-        System.out.println("2. Старт метода с двумя потоками.");
+        System.out.println("2. Старт метода с четыремя потоками.");
 
         float[] arr = new float[size];
-        float[] arr1 = new float[half_size];
-        float[] arr2 = new float[half_size];
+        float[] arr1 = new float[quarter_size];
+        float[] arr2 = new float[quarter_size];
+        float[] arr3 = new float[quarter_size];
+        float[] arr4 = new float[quarter_size];
+
         Arrays.fill(arr, 1.0f);
 
         long start = System.currentTimeMillis();
-        System.arraycopy(arr, 0, arr1, 0, half_size);
-        System.arraycopy(arr, half_size, arr2, 0, half_size);
+        System.arraycopy(arr, 0, arr1, 0, quarter_size);
+        System.arraycopy(arr, quarter_size, arr2, 0, quarter_size);
+        System.arraycopy(arr, quarter_size, arr3, 0, quarter_size);
+        System.arraycopy(arr, quarter_size, arr4, 0, quarter_size);
         long split = System.currentTimeMillis();
-        System.out.println("Время выполнения на разделение массива: " + (split - start) + " миллисекунд.");
+        System.out.println("Время выполнения разделения одного массива на четыре: " + (split - start) + " миллисекунд.");
 
-        Thread t_1 = new Thread(() -> this.twoThread(arr1, 1));
-        Thread t_2 = new Thread(() -> this.twoThread(arr2, 2));
+        Thread t_1 = new Thread(() -> this.fourThread(arr1, 1));
+        Thread t_2 = new Thread(() -> this.fourThread(arr2, 2));
+        Thread t_3 = new Thread(() -> this.fourThread(arr3, 3));
+        Thread t_4= new Thread(() -> this.fourThread(arr4, 4));
 
         t_1.start();
         t_2.start();
+        t_3.start();
+        t_4.start();
 
-        t_1.join();
-        t_2.join();
+        if (t_1.isAlive()) t_1.join();
+        if (t_2.isAlive()) t_2.join();
+        if (t_3.isAlive()) t_3.join();
+        if (t_4.isAlive()) t_4.join();
 
         long connect = System.currentTimeMillis();
-        System.arraycopy(arr1, 0, arr, 0, half_size);
-        System.arraycopy(arr2, 0, arr, half_size, half_size);
+        System.arraycopy(arr1, 0, arr, 0, quarter_size);
+        System.arraycopy(arr2, 0, arr, quarter_size, quarter_size);
+        System.arraycopy(arr3, 0, arr, quarter_size, quarter_size);
+        System.arraycopy(arr4, 0, arr, quarter_size, quarter_size);
         long end = System.currentTimeMillis();
-        System.out.println("Время выполнения на склеивание двух массивов в один: " + (end - connect) + " миллисекунд.");
+        System.out.println("Время выполнения на склеивание четырех массивов в один: " + (end - connect) + " миллисекунд.");
 
-        System.out.println("Завершение метода 2. Время выполнения программы с двумя потоками: " + (end - start) + " миллисекунд.");
+        System.out.println("Время выполнения программы с четыремя потоками: " + (end - start) + " миллисекунд.");
+        System.out.println("Завершение метода 2.");
 
     }
 
-    protected void twoThread(float[] arr, int number) {
+    protected void fourThread(float[] arr, int number) {
 
         long start = System.currentTimeMillis();
 
